8d1916e9f6b0e99e39de1ebde8e217114ea40425
TANMAY SINGH <ts7583@srmist.edu.in>
2026-05-05 20:46:48 +0530

    UC3: Add failing test cases for generic Length equality

--- src/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -5,40 +5,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class QuantityMeasurementAppTest {
 
-    // TC1: Same value should be equal
+    // ==================== UC1: Feet Equality Tests ====================
+
+    // TC1: Same feet value should be equal
     @Test
     public void testFeetEquality_SameValue() {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
-        QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(1.0);
+        Length feet1 = new Length(1.0, LengthUnit.FEET);
+        Length feet2 = new Length(1.0, LengthUnit.FEET);
         assertTrue(feet1.equals(feet2));
     }
 
-    // TC2: Different values should not be equal
+    // TC2: Different feet values should not be equal
     @Test
     public void testFeetEquality_DifferentValue() {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
-        QuantityMeasurementApp.Feet feet2 = new QuantityMeasurementApp.Feet(2.0);
+        Length feet1 = new Length(1.0, LengthUnit.FEET);
+        Length feet2 = new Length(2.0, LengthUnit.FEET);
         assertFalse(feet1.equals(feet2));
     }
 
-    // TC3: Comparison with null should return false
+    // TC3: Feet comparison with null should return false
     @Test
     public void testFeetEquality_NullComparison() {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
+        Length feet1 = new Length(1.0, LengthUnit.FEET);
         assertFalse(feet1.equals(null));
     }
 
-    // TC4: Comparison with a different class object should return false
+    // TC4: Feet comparison with different class should return false
     @Test
     public void testFeetEquality_DifferentClass() {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
-        assertFalse(feet1.equals("not a feet object"));
+        Length feet1 = new Length(1.0, LengthUnit.FEET);
+        assertFalse(feet1.equals("not a length object"));
     }
 
-    // TC5: Same reference should be equal
+    // TC5: Same feet reference should be equal
     @Test
     public void testFeetEquality_SameReference() {
-        QuantityMeasurementApp.Feet feet1 = new QuantityMeasurementApp.Feet(1.0);
+        Length feet1 = new Length(1.0, LengthUnit.FEET);
         assertTrue(feet1.equals(feet1));
     }
 
@@ -47,37 +49,63 @@ public class QuantityMeasurementAppTest {
     // TC6: Same inch value should be equal
     @Test
     public void testInchesEquality_SameValue() {
-        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
-        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(1.0);
+        Length inches1 = new Length(1.0, LengthUnit.INCHES);
+        Length inches2 = new Length(1.0, LengthUnit.INCHES);
         assertTrue(inches1.equals(inches2));
     }
 
     // TC7: Different inch values should not be equal
     @Test
     public void testInchesEquality_DifferentValue() {
-        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
-        QuantityMeasurementApp.Inches inches2 = new QuantityMeasurementApp.Inches(2.0);
+        Length inches1 = new Length(1.0, LengthUnit.INCHES);
+        Length inches2 = new Length(2.0, LengthUnit.INCHES);
         assertFalse(inches1.equals(inches2));
     }
 
-    // TC8: Comparison with null should return false
+    // TC8: Inches comparison with null should return false
     @Test
     public void testInchesEquality_NullComparison() {
-        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        Length inches1 = new Length(1.0, LengthUnit.INCHES);
         assertFalse(inches1.equals(null));
     }
 
-    // TC9: Comparison with a different class object should return false
+    // TC9: Inches comparison with different class should return false
     @Test
     public void testInchesEquality_DifferentClass() {
-        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
-        assertFalse(inches1.equals("not an inches object"));
+        Length inches1 = new Length(1.0, LengthUnit.INCHES);
+        assertFalse(inches1.equals("not a length object"));
     }
 
-    // TC10: Same reference should be equal
+    // TC10: Same inches reference should be equal
     @Test
     public void testInchesEquality_SameReference() {
-        QuantityMeasurementApp.Inches inches1 = new QuantityMeasurementApp.Inches(1.0);
+        Length inches1 = new Length(1.0, LengthUnit.INCHES);
         assertTrue(inches1.equals(inches1));
+    }
+
+    // ==================== UC3: Cross-Unit Equality Tests ====================
+
+    // TC11: 1 foot == 12 inches
+    @Test
+    public void testLengthEquality_OneFoot_EqualTo_TwelveInches() {
+        Length oneFoot   = new Length(1.0, LengthUnit.FEET);
+        Length twelveIn  = new Length(12.0, LengthUnit.INCHES);
+        assertTrue(oneFoot.equals(twelveIn));
+    }
+
+    // TC12: 12 inches == 1 foot (symmetric)
+    @Test
+    public void testLengthEquality_TwelveInches_EqualTo_OneFoot() {
+        Length twelveIn = new Length(12.0, LengthUnit.INCHES);
+        Length oneFoot  = new Length(1.0, LengthUnit.FEET);
+        assertTrue(twelveIn.equals(oneFoot));
+    }
+
+    // TC13: 1 foot != 1 inch
+    @Test
+    public void testLengthInequality_OneFoot_NotEqualTo_OneInch() {
+        Length oneFoot = new Length(1.0, LengthUnit.FEET);
+        Length oneInch = new Length(1.0, LengthUnit.INCHES);
+        assertFalse(oneFoot.equals(oneInch));
     }
 }
