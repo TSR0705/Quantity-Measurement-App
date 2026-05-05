f9cd80cb867b15de2f568ffbda93caa70477e7cb
TANMAY SINGH <ts7583@srmist.edu.in>
2026-05-05 20:48:20 +0530

    UC3: Refactor to Length class with unit conversion

--- src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,40 +1,6 @@
 package com.apps.quantitymeasurement;
 
 public class QuantityMeasurementApp {
-
-    public static class Feet {
-
-        private final double value;
-
-        public Feet(double value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null) return false;
-            if (getClass() != obj.getClass()) return false;
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
-    }
-
-    public static class Inches {
-
-        private final double value;
-
-        public Inches(double value) {
-            this.value = value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null) return false;
-            if (getClass() != obj.getClass()) return false;
-            Inches other = (Inches) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
-    }
+    // UC1 and UC2 Feet/Inches classes replaced by Length + LengthUnit in UC3.
+    // All measurement logic is now handled by the Length class.
 }
